818d740cd3c7c79bf784814989c1cc703f7f9387
sky0621 <sky14work@gmail.com>
2017-12-13 02:02:01 +0900

    excep

---
+++ src/ch02b/list0225.kt
@@ -0,0 +1,13 @@
+package ch02b
+
+/**
+ * Created by SS on 2017/12/13.
+ */
+fun main(args: Array<String>) {
+    var number: Int = 0
+    val percentage =
+        if (number in 0..100)
+            number
+    else
+        throw IllegalArgumentException("A percentage value must be between 0 and 100: $number")
+}
