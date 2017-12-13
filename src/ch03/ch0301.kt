f0965d42609665ab94974255942e4405791ae179
sky0621 <sky14work@gmail.com>
2017-12-14 02:30:41 +0900

    ch0301

---
+++ src/ch03/ch0301.kt
@@ -0,0 +1,15 @@
+package ch03
+
+/**
+ * Created by SS on 2017/12/14.
+ */
+fun main(args: Array<String>) {
+    val set = hashSetOf(1, 7, 53)
+    println(set)
+
+    val list = arrayListOf(2, 8, 54)
+    println(list)
+
+    val map = hashMapOf(3 to "Kotlin", 9 to "Golang", 55 to "Scala")
+    println(map)
+}
