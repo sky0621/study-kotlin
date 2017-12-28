c299003da622b6ddfc470d4d6665f9580a03cc4c
sky0621 <sky14work@gmail.com>
2017-12-29 02:05:46 +0900

    re

---
+++ src/ch03/ch0302.kt
@@ -0,0 +1,8 @@
+package ch03
+
+/**
+ * Created by SS on 2017/12/29.
+ */
+fun main(arg: Array<String>) {
+
+}
---
+++ src/re/re.kt
@@ -0,0 +1,16 @@
+package re
+
+/**
+ * Created by SS on 2017/12/29.
+ */
+fun main(arg: Array<String>) {
+    println("Hello, Kotlin.")
+
+    val age = 39
+    val age2 = age * 2
+    println(age2)
+
+    var age3 = 25
+    age3 = age3 * 2
+    println(age3)
+}
